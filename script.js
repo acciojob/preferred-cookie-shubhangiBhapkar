2c40c1a821bf66e90665ba6371ddb6425fec1ff2
Shubhangi Bhapkar <shubhangibhapkar35@gmail.com>
2026-08-18 16:01:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+const form = document.getElementById("myform");
+form.addEventListener("submit",function(e){
+	e.preventDefault();
+
+	const fontsize = document.getElementById("fontsize").value;
+	const fontcolor = document.getElementById("fontcolor").value;
+
+	document.cookie = `fontcolor=${fontcolor}`;
+	document.cookie = `fontsize=${fontsize}`;
+
+}
+
+const cookies = document.cookie.split(";");
+const fontCookie = cookies.find(cookie => cookie.startWith("fontcolor="));
+const fontcolor = fontCookie.split("=").[1];
+document.documentElement.style.setProperty("--fontcolor", fontcolor);
+
+const fontCookie = cookies.find(cookie => cookie.startWith("fontsize="));
+const fontsize = fontCookie.split("=").[1];
+document.documentElement.style.setProperty("--fontsize", fontsize);
+
+
+					  
