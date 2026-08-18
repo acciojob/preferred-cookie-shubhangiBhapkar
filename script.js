bb2ab4ae0eae92df2dbc08691db7676f189c411c
Shubhangi Bhapkar <shubhangibhapkar35@gmail.com>
2026-08-18 16:38:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,14 +11,36 @@ form.addEventListener("submit",function(e){
 
 }
 
-const cookies = document.cookie.split(";");
-const fontCookie = cookies.find(cookie => cookie.startWith("fontcolor="));
-const fontcolor = fontCookie.split("=").[1];
-document.documentElement.style.setProperty("--fontcolor", fontcolor);
+document.addEventListener("DOMContentLoaded", () => {
 
-const fontCookie = cookies.find(cookie => cookie.startWith("fontsize="));
-const fontsize = fontCookie.split("=").[1];
-document.documentElement.style.setProperty("--fontsize", fontsize);
+    const cookies = document.cookie.split("; ");
 
+    const fontCookie = cookies.find(cookie =>
+        cookie.startsWith("fontcolor=")
+    );
+
+    if (fontCookie) {
+        const fontcolor = fontCookie.split("=")[1];
+
+        document.documentElement.style.setProperty(
+            "--fontcolor",
+            fontcolor
+        );
+    }
+
+	
+    const fontsize = cookies.find(cookie =>
+        cookie.startsWith("fontsize=")
+    );
+
+    if (fontsize) {
+        const fontsize = fontCookie.split("=")[1];
+
+        document.documentElement.style.setProperty(
+            "--fontsize",
+            fontsize
+        );
+    }
+});
 
 					  
